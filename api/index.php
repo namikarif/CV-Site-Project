fa7cee8dc0b72bcf76c5fdf46750bcbcf04ff7ba
Namig <45629136+namikarif@users.noreply.github.com>
2021-11-09 23:51:31 +0300

    Delete api directory

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-date_default_timezone_set('Europe/Istanbul');
-//$now = strftime("%Y-%m-%d %H:%M:%S");
-$now = strftime("%Y-%m-%d");
-echo $now;
-//require "./core/function.php";
-//$functions = new FunctionClass();
-//echo $functions->sendSms('994502004200', $text = 'Hörmətli müştəri qeydiyyatınız uğurla tamamlanmışdır. Müştəri kodunuz: 100001');
